c955b9ae2cf486ae58fab9bce5c168f35d201d3d
Keverton Reis <kevertondrv@gmail.com>
2026-03-02 17:45:31 -0300

    include form login initial

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -13,23 +13,23 @@ const Menu = () => {
       <aside className={styles.sidebar}>
         <h2>Menu</h2>
 
-        <button onClick={() => navigate("/cadcliente")}>
+        <button onClick={() => navigate("/layoutprincipal/cadcliente")}>
           Cadastro de cliente
         </button>
 
-        <button onClick={() => navigate("/cadproposta")}>
+        <button onClick={() => navigate("/layoutprincipal/cadproposta")}>
           Cadastro inicial da proposta
         </button>
 
-        <button onClick={() => navigate("/orcamento")}>
+        <button onClick={() => navigate("/layoutprincipal/orcamento")}>
           Orçamento e finalização da proposta
         </button>
 
-        <button onClick={() => navigate("/credencial")}>
+        <button onClick={() => navigate("/layoutprincipal/credencial")}>
           Carta de credenciamento
         </button>
 
-        <button onClick={() => navigate("/diversos")}>
+        <button onClick={() => navigate("/layoutprincipal/diversos")}>
           Cadastros diversos
         </button>
       </aside>
